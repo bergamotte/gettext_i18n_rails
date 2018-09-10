28b2f60cfbd2efba846cc456cec77c50c3e8cc42
Simon Rand <simonrand@gmail.com>
2018-09-10 17:19:06 +0100

    Start to refactor RubyGettextExtractor to use GetText::POEntry objects so we can support context and (our version of) HAML comments

--- lib/gettext_i18n_rails/ruby_gettext_extractor.rb
+++ lib/gettext_i18n_rails/ruby_gettext_extractor.rb
@@ -25,6 +25,8 @@ module RubyGettextExtractor
     attr_reader :results
 
     def initialize(targets)
+      # TODO: At this point it's unclear what the targets arg does, need to
+      #       investigate this further.
       @targets = {}
       @results = []
 
@@ -85,6 +87,23 @@ module RubyGettextExtractor
       key.gsub("\n", '\n').gsub("\t", '\t').gsub("\0", '\0')
     end
 
+    def store_po_entry(po_entry)
+      existing_entry = @results.find { |result| result.mergeable?(po_entry) }
+
+      if existing_entry.nil?
+        # TODO: Targets?
+        @results << po_entry && return
+      end
+
+      # NOTE: POEntry#merge concats comments, which we don't want, so we'll
+      #       manage the merge manually.
+      existing_entry.references.concat(po_entry.references)
+      comment = po_entry.extracted_comment
+      unless !comment.nil? && !existing_entry.extracted_comment.nil? && existing_entry.extracted_comment.include?(comment)
+        existing_entry.add_comment(comment)
+      end
+    end
+
     def store_key(key, args)
       if key
         res = @targets[key]
@@ -99,20 +118,38 @@ module RubyGettextExtractor
       end
     end
 
+    def build_po_entry(type, id, context, file, line, comment = nil)
+      entry = GetText::POEntry.new(type)
+      entry.msgctxt = context unless context.nil?
+      if type == :plural && id.kind_of?(Array)
+        entry.msgid_plural = id[1]
+        id = id[0]
+      end
+      entry.msgid = id
+      entry.references = ["#{file}:#{line}"]
+      entry.add_comment(comment) if comment
+      entry
+    end
+
     def gettext_simple_call(args)
       # args comes in 2 forms:
       #   s(s(:str, "Button Group Order:"))
       #   s(:str, "Button Group Order:")
       # normalizing:
+      comment = args[1][1] if args[1]
       args = args.first if Sexp === args.sexp_type
+      store_po_entry(build_po_entry(:normal, args[1], nil, args.file, args.line, comment))
+    end
 
-      key  = extract_key_singular(args, "\004")
-      store_key(key, args)
+    def gettext_context_call(args)
+      comment = args[2] ? args[2][1] : nil
+      store_po_entry(build_po_entry(:msgctxt, args[1][1], args[0][1], args.file, args.line, comment))
     end
 
     def gettext_plural_call(args)
-      key = extract_key_plural(args, "\000")
-      store_key(key, args)
+      comment = args[3] ? args[3][1] : nil
+      # TODO: This doesn't support context yet (we'll use `np_` for that)
+      store_po_entry(build_po_entry(:plural, [args[0][1], args[1][1]], nil, args.file, args.line, comment))
     end
 
     def process_call exp
@@ -121,8 +158,10 @@ module RubyGettextExtractor
       meth  = exp.shift
 
       case meth
-      when :_, :p_, :N_, :pgettext, :s_
+      when :_, :N_, :s_
         gettext_simple_call(exp)
+      when :p_, :pgettext
+        gettext_context_call(exp)
       when :n_
         gettext_plural_call(exp)
       end
